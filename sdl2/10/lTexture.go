7c312714334cac055a872e4f0d4f96c611f256a8
Jake Casaletto <jake.m.casaletto@gmail.com>
2018-05-14 00:55:32 -0700

    chapter 10 fix free function not reinitizalizing variablesies

--- sdl2/10/lTexture.go
+++ sdl2/10/lTexture.go
@@ -17,6 +17,8 @@ func (l *LTexture) Free() error {
 	if l.mTexture != nil {
 		if err := l.mTexture.Destroy(); err != nil {
 			l.mTexture = nil
+			l.mWidth = 0
+			l.mHeight = 0
 			return err
 		}
 	}
